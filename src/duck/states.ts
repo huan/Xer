7fbeaf2f08b777ef20f509021c54f035a654faf0
Huan (李卓桓) <zixia@zixia.net>
2022-10-02 03:55:10 +0800

    add State, Event, and Type enum

---
+++ src/duck/states.ts
@@ -0,0 +1,68 @@
+/**
+ *   Xer is a dating bot target for getting more conversions from online dating apps
+ *
+ *   @copyright 2022 Xuan Jiang <https://github.com/xjiang67>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+export const Unknown = 'xer/Unknown'
+
+export const Listening   = 'xer/Listening'
+export const Thinking    = 'xer/Thinking'
+export const Feedbacking = 'xer/Feedbacking'
+export const Feedbacked  = 'xer/Feedbacked'
+export const Checking    = 'xer/Checking'
+export const Validating  = 'xer/Validating'
+
+export const Active = 'xer/Active'
+export const Inactive = 'xer/Inactive'
+
+export const Recognizing  = 'xer/Recognizing'
+export const Recognized   = 'xer/Recognized'
+
+export const Classifying  = 'bot5-assisstant/Classifying'
+export const Classified   = 'xer/Classified'
+
+export const Processing = 'xer/Processing'
+export const Delivering = 'xer/Delivering'
+
+export const Mentioning   = 'xer/Mentioning'
+export const Registering  = 'xer/Registering'
+export const Registered   = 'xer/Registered'
+
+export const Saying         = 'xer/Saying'
+export const Updating       = 'xer/Updating'
+export const Confirming     = 'xer/Confirming'
+
+export const Understanding  = 'xer/Understanding'
+export const Understood     = 'xer/Understood'
+
+export const Introducing  = 'xer/Introducing'
+export const Selecting    = 'xer/Selecting'
+export const Scheduling   = 'xer/Scheduling'
+export const Noticing     = 'xer/Noticing'
+export const Reporting    = 'xer/Reporting'
+export const Parsing      = 'xer/Parsing'
+
+export const Loading      = 'xer/Loading'
+export const Loaded      = 'xer/Loaded'
+
+export const Messaging    = 'xer/Messaging'
+export const Filing       = 'xer/Filing'
+
+export const Textualizing = 'xer/Textualizing'
+export const Textualized  = 'xer/Textualized'
+
+export const Nexting      = 'xer/Nexting'
+export const Batching     = 'xer/Batching'
---
+++ src/duck/types.ts
@@ -0,0 +1,115 @@
+/**
+ *   Xer is a dating bot target for getting more conversions from online dating apps
+ *
+ *   @copyright 2022 Xuan Jiang <https://github.com/xjiang67>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+export const MESSAGE = 'xer/MESSAGE'
+
+export const ROOM = 'xer/ROOM'
+export const NO_ROOM = 'xer/NO_ROOM'
+
+export const ADMINS    = 'xer/ADMINS'
+export const CHAIRS    = 'xer/CHAIRS'
+export const TALKS     = 'xer/TALKS'
+export const MEMBERS   = 'xer/MEMBERS'
+export const ATTENDEES = 'xer/ATTENDEES'
+
+export const TALK     = 'xer/TALK'
+export const NO_TALK  = 'xer/NO_TALK'
+
+export const NO_MEMBER  = 'xer/NO_MEMBER'
+export const NO_AUDIO   = 'xer/NO_AUDIO'
+
+export const CONTACTS   = 'xer/CONTACTS'
+export const NO_CONTACT = 'xer/NO_CONTACT'
+
+export const TEXT     = 'xer/TEXT'
+export const NO_TEXT  = 'xer/NO_TEXT'
+
+export const MENTIONS = 'xer/MENTIONS'
+export const NO_MENTION = 'xer/NO_MENTION'
+
+export const BACK = 'xer/BACK'
+export const NEXT = 'xer/NEXT'
+
+export const START = 'xer/START'
+export const STOP = 'xer/STOP'
+
+export const SAY = 'xer/SAY'
+export const RESET = 'xer/RESET'
+
+export const GERROR = 'xer/GERROR'
+
+export const FEEDBACKS = 'xer/FEEDBACKS'
+export const FEEDBACK = 'xer/FEEDBACK'
+
+/**
+ * Complete v.s. Finish
+ *  @see https://ejoy-english.com/blog/complete-vs-finish-similar-but-different/
+ */
+export const FINISH = 'xer/FINISH'
+export const COMPLETE = 'xer/COMPLETE'
+
+/**
+ * Abort v.s. Cancel
+ *  @see https://stackoverflow.com/a/9838022/1123955
+ */
+export const ABORT = 'xer/ABORT'
+export const CANCEL = 'xer/CANCEL'
+
+export const WECHATY = 'xer/WECHATY'
+
+export const DELIVER = 'xer/DELIVER'
+export const INTENTS = 'xer/INTENTS'
+
+export const HELP = 'xer/HELP'
+export const REPORT = 'xer/REPORT'
+
+export const IDLE = 'xer/IDLE'
+export const CHECK = 'xer/CHECK'
+
+export const PROCESS = 'xer/PROCESS'
+export const PARSE = 'xer/PARSE'
+export const NOTICE = 'xer/NOTICE'
+
+/**
+ * Minutes of Meeting (MoM)
+ *  @link https://en.wikipedia.org/wiki/Minutes
+ */
+export const MINUTES = 'xer/MINUTES'
+
+export const CONVERSATION = 'xer/CONVERSATION'
+export const NOP = 'xer/NOP'
+
+export const FILE    = 'xer/FILE'
+export const NO_FILE = 'xer/NO_FILE'
+
+export const LOAD = 'bot5-assisstant/LOAD'
+
+export const REGISTER = 'xer/REGISTER'
+
+export const CHECKIN = 'xer/CHECKIN'
+
+export const ADD_CONTACT = 'xer/ADD_CONTACT'
+
+export const TEST = 'xer/TEST'
+export const VALIDATE = 'xer/VALIDATE'
+
+export const BATCH = 'xer/BATCH'
+
+export const DATES    = 'xer/DATES'
+export const DATE     = 'xer/DATE'
+export const NO_DATE  = 'xer/NO_DATE'
